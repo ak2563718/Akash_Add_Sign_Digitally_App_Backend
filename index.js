2cf6ed1d41d6631229f39327c3fd1421fdcb0faf
ak2563718 <ak2563718@gmail.com>
2026-06-12 16:36:57 +0530

    New commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
-    origin:['http://localhost:3000','http://192.168.155.104:3000','https://akash-add-sign-digitally-app-fronte.vercel.app/forgot-password'],
+    origin:['http://localhost:3000','http://192.168.155.104:3000','https://akash-add-sign-digitally-app-fronte.vercel.app'],
     credentials:true,
 }))
 app.use(cookieParser());
